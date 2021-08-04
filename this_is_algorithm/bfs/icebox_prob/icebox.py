96aa36903869db2e0d6655ca94e0ffd1ea7fe682
ManduTheCat <krocd@naver.com>
2021-08-04 13:17:54 +0900

    make bfs recursive function in icebox prob

--- this_is_algorithm/bfs/icebox_prob/icebox.py
+++ this_is_algorithm/bfs/icebox_prob/icebox.py
@@ -1,14 +1,19 @@
-from collections import deque
-
-def bfs(graph, start, flag):
-	que = deque()
-	graph[start] = False
-
+def bfs(icebox, x, y, n, m):
+	if x <= -1 or x >= n or y <= -1 or y > m:
+		return False
+	if icebox[x][y] == 0:
+		icebox = 1
+		bfs(x - 1, y)
+		bfs(x + 1, y)
+		bfs(x, y - 1)
+		bfs(x, y + 1)
+		
+		return True
 
 def main():
 	n, m = map(int, input().split())
 	icebox = []
-	flag = [False] * (n * m)
+	result = 0
 
 	for i in range(n):
 		icebox.append(list(map(int, input().split())))
