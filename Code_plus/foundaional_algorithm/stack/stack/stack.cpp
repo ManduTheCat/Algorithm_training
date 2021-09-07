be15ef81cb279052b0752935bd02c64432be8e24
KMJ <krocd@naver.com>
2021-09-08 04:51:50 +0900

    solved

--- Code_plus/foundaional_algorithm/stack/stack/stack.cpp
+++ Code_plus/foundaional_algorithm/stack/stack/stack.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+// #include <string>
 
 void push(int *stack, int *size, int num)
 {
@@ -41,44 +41,65 @@ int empty( int *size)
 
 int top(int *stack, int *size)
 {
+	if(*size == 0)
+		return -1;
 	return stack[*size - 1];
 }
 
 int main()
 {
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);
-	std::cout.tie(NULL);
+	// std::ios_base::sync_with_stdio(false);
+	// std::cin.tie(NULL);
+	// std::cout.tie(NULL);
 
 	int num_of_command;
 	int num;
 	std::string command;
+	std::string s_push = "push";
+	std::string s_pop = "pop";
+	std::string s_empty = "empty";
+	std::string s_size = "size";
+	std::string s_top = "top";
 	int *stack;
 	int  stack_size;
+	int times;
 
-	// std::cin >> num_of_command;
-	// std::cin >> command >> num;
 
-	num_of_command = 10;
+	std::cin >> num_of_command;
 	stack = new int[num_of_command];
 	stack_size = 0;
+	times = 0;
 
-	push(stack, &stack_size, 12); //1 //0
-	std::cout << top(stack, &stack_size) << '\n';
-	push(stack, &stack_size, 9);
-	std::cout <<top(stack, &stack_size) << '\n'; //2 //1
-	push(stack, &stack_size, 8); //size 3 //index 2
-	std::cout << top(stack, &stack_size) << '\n';
+	while (times < num_of_command)
+	{
+		std:: cin >> command;
+		if(!(command.compare(s_push)))
+		{
+			std::cin >> num;
+			push(stack, &stack_size, num);
+		}
 
-	std::cout << "pop :" << pop(stack, &stack_size) << '\n'; //size 3 -> 2 index 2
-	std::cout << "top :" <<  top(stack, &stack_size) << '\n';
-	std::cout << "empty :" << empty(&stack_size) << '\n';
-	std::cout << "size :" << size(&stack_size) << '\n';
+		else if(!(command.compare(s_pop)))
+		{
+			std::cout << pop(stack, &stack_size) << '\n';
+		}
 
-	std::cout << "pop :" << pop(stack, &stack_size) << '\n'; //size 3 -> 2 index 2
-	std::cout << "pop :" << pop(stack, &stack_size) << '\n'; //size 3 -> 2 index 2
-	std::cout << "pop :" << pop(stack, &stack_size) << '\n'; //size 3 -> 2 index 2
+		else if(!(command.compare(s_empty)))
+		{
+			std::cout << empty(&stack_size) << '\n';
+		}
 
-	std::cout << "empty :" << empty(&stack_size) << '\n';
-	std::cout << "size :" << size(&stack_size) << '\n';
+		else if(!(command.compare(s_size)))
+		{
+			std::cout << size(&stack_size) << '\n';
+		}
+
+		else if(!(command.compare(s_top)))
+		{
+			std::cout << top(stack, &stack_size) << '\n';
+		}
+
+		times++;
+	}
+
 }
