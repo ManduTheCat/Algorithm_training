e050993623ee1ff3ab94c91078edfd9c30151b11
KMJ <krocd@naver.com>
2021-09-08 04:01:10 +0900

    will make given command

--- Code_plus/foundaional_algorithm/stack/stack/stack.cpp
+++ Code_plus/foundaional_algorithm/stack/stack/stack.cpp
@@ -5,28 +5,43 @@ void push(int *stack, int *size, int num)
 {
 	stack[*size] = num;
 	*size = *size + 1;
-	std::cout<< "size :" << *size <<'\n';
-
 }
 
-int pop(int *stack, int *size, int num)
+int pop(int *stack, int *size)
 {
+	int i;
 
+	if (*size == 0)
+	{
+		return -1;
+	}
+
+	i = *size - 1;
+	*size -= 1;
+	return stack[i];
 }
 
-int size()
+int size(int *size)
 {
-
+	return *size;
 }
 
-int empty()
+int empty( int *size)
 {
+	if(*size == 0)
+	{
+		return 1;
+	}
 
+	else
+	{
+		return 0;
+	}
 }
 
-int top()
+int top(int *stack, int *size)
 {
-
+	return stack[*size - 1];
 }
 
 int main()
@@ -48,12 +63,22 @@ int main()
 	stack = new int[num_of_command];
 	stack_size = 0;
 
-	push(stack, &stack_size, 12);
+	push(stack, &stack_size, 12); //1 //0
+	std::cout << top(stack, &stack_size) << '\n';
 	push(stack, &stack_size, 9);
-	push(stack, &stack_size, 8);
+	std::cout <<top(stack, &stack_size) << '\n'; //2 //1
+	push(stack, &stack_size, 8); //size 3 //index 2
+	std::cout << top(stack, &stack_size) << '\n';
 
-	std::cout << stack[0] << '\n';
-	std::cout << stack[1] << '\n';
-	std::cout << stack[2] << '\n';
+	std::cout << "pop :" << pop(stack, &stack_size) << '\n'; //size 3 -> 2 index 2
+	std::cout << "top :" <<  top(stack, &stack_size) << '\n';
+	std::cout << "empty :" << empty(&stack_size) << '\n';
+	std::cout << "size :" << size(&stack_size) << '\n';
 
+	std::cout << "pop :" << pop(stack, &stack_size) << '\n'; //size 3 -> 2 index 2
+	std::cout << "pop :" << pop(stack, &stack_size) << '\n'; //size 3 -> 2 index 2
+	std::cout << "pop :" << pop(stack, &stack_size) << '\n'; //size 3 -> 2 index 2
+
+	std::cout << "empty :" << empty(&stack_size) << '\n';
+	std::cout << "size :" << size(&stack_size) << '\n';
 }
