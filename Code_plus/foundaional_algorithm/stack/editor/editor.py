90402bcaf8eeafe18d0105460cfea9290f234d29
KMJ <krocd@naver.com>
2021-09-12 17:01:31 +0900

    solve editor

--- Code_plus/foundaional_algorithm/stack/editor/editor.py
+++ Code_plus/foundaional_algorithm/stack/editor/editor.py
@@ -1,26 +1,37 @@
+from sys import stdin
+
 def main():
-	input_str = list(input())
+	first_stack = list(stdin.readline().strip())
 	max_cmd_num = int(input())
-	input_list = list(input() for _ in range(max_cmd_num))
+	input_list = list()
+	for _ in range(max_cmd_num):
+		input_list.append(stdin.readline().strip())
+	print(input_list)
 	second_stack = list()
 
 	for input_cmd in input_list:
-		if len(input_cmd) == 2:
-			input_str.append(input_cmd[1])
+		if len(input_cmd) != 1:
+			first_stack.append(input_cmd[-1])
 		else:
 			if input_cmd == "L":
-				if len(input_str) != 0:
-					temp = input_str.pop()
+				if len(first_stack) != 0:
+					temp = first_stack.pop()
 					second_stack.append(temp)
 			elif input_cmd == "D":
 				if len(second_stack) != 0:
 					temp = second_stack.pop()
-					input_str.append("temp")
+					first_stack.append(temp)
 			elif input_cmd == "B":
-				input_str.pop()
+				if len(first_stack) != 0:
+					first_stack.pop()
 
-	print(input_cmd + second_stack)
+	# print(first_stack ,second_stack)
 
+
+	for charc in first_stack:
+		print(charc, end = "")
+	for charc in reversed (second_stack):
+		print(charc, end = "")
 
 
 if __name__ == "__main__":
