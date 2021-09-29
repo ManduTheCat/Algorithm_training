8cdc71a5686a57acce5c63114670319e9ac3c9ca
KMJ <krocd@naver.com>
2021-09-29 20:49:50 +0900

    solved make sum pob

--- Code_plus/foundaional_algorithm/dynamic/make_sum_num/make_sum_num.cpp
+++ Code_plus/foundaional_algorithm/dynamic/make_sum_num/make_sum_num.cpp
@@ -5,17 +5,33 @@ using namespace std;
 
 vector <int> dp(15,0);
 
+int make_num(int n)
+{
+	if(dp[n] != 0)
+	{
+		return dp[n];
+	}
+	else if(dp[n] == 0)
+		dp[n] = make_num(n - 3)+ make_num(n - 2) + make_num(n - 1);
+
+	return dp[n];
+}
+
+
 int main()
 {
 	int n;
-	cin  >> n;
+	int cmd_count;
+	cin >> cmd_count;
+	while(cmd_count--)
+	{
+		cin  >> n;
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 2; //1,1 2
+		dp[3] = 4;//111 12 3 21
+		dp[4] = 7;
 
-	dp[0] = 0;
-	dp[1] = 1;
-	dp[2] = 2; //1,1 2
-	dp[3] = 4;//111 12 3 21
-
-	dp[n] = 1 + dp [n -1]
-
-
+		cout <<	make_num(n) << "\n";
+	}
 }
