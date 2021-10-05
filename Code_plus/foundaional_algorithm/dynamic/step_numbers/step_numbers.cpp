d707fa91fd198ec78f0b96654f6482a8b25b875b
KMJ <krocd@naver.com>
2021-10-05 14:57:03 +0900

    solved by top down 마지막 나머지 연산 은로 해결

--- Code_plus/foundaional_algorithm/dynamic/step_numbers/step_numbers.cpp
+++ Code_plus/foundaional_algorithm/dynamic/step_numbers/step_numbers.cpp
@@ -1,60 +1,50 @@
 #include <iostream>
-#include <vector>
+#include <string.h>
 using namespace std;
 
-#define MODENUM 1000000000;
+#define MOD 1000000000;
 
-vector <int> dp(101, 0);
-int numbers[10][2];
 
-long long step_numbers(long long N)
+int dp[101][10];
+
+int step_num(int n, int l)
 {
-	long long count = 0;
-	if(N == 1)
+	int &ref = dp[n][l];
+	if(n == 1)
 	{
-		return 9;
+		if (l == 0)
+			return 0;
+		else
+			return 1;
 	}
-		for(int i = 0; i <= 9; i++)
-		{
-			for(int j = 1; j < N; j++)
-			{
-				for(int k = 1; k <= 2; k++)
-				{
-					if(numbers[i][k] == 9)
-					{
-						numbers[i][k] = i - 1;
-						count+=1;
-						count%= MODENUM;
-					}
-					else if(numbers[i][k] <= 1)
-					{
-						numbers[i][k] = i + 1;
-						count+=1;
-						count%= MODENUM;
-					}
-					else
-					{
-						numbers[i][2] = i + 1;
-						numbers[i][1] = i - 1;
-						count+=2;
-						count%= MODENUM;
-					}
-				}
-			}
-		}
-	return count;
+	if(ref != -1)
+		return ref;
+	if(l == 0)
+	{
+		return ref = step_num(n - 1, 1) % MOD;
+	}
+	if(l == 9)
+	{
+		return ref = step_num(n - 1, 8) % MOD;
+	}
+	else
+		return ref =  (step_num(n -1 , l + 1) + step_num(n - 1, l - 1)) % MOD;
+
 }
 
 int main()
 {
-	int n;
-	cin >> n;
-	for(int i = 0; i <= 10; i++)
-	{
-		numbers[i][1] = 1;
-		numbers[i][2] = 1;
-	}
+	int sum = 0;
+	int n ;
 
-	cout << step_numbers(n) << "\n";
+	cin >> n;
+	memset(dp, -1, sizeof(dp));
+	for(int i = 0; i <= 9; i++)
+	{
+		sum += step_num(n, i);
+		sum %= MOD ;//이거추가하니 해결됨 띠벌
+	}
+	cout << sum << "\n";
 	return 0;
 }
+
