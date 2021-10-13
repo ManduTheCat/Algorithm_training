ebbffc4610b9b2d431e92955806e69fa17c16e37
KMJ <krocd@naver.com>
2021-10-13 18:16:45 +0900

    change data structur vector to list

--- Code_plus/foundaional_algorithm/string_process/ant/ant.cpp
+++ Code_plus/foundaional_algorithm/string_process/ant/ant.cpp
@@ -1,32 +1,62 @@
 #include <iostream>
-#include <vector>
-
+#include <algorithm>
+#include <list>
 using namespace std;
 
 int main()
 {
 	int first_string_size;
 	int second_string_size;
+	int sec;
+	char c;
 
 	cin >> first_string_size;
 	cin >> second_string_size;
+	cin >> sec;
 
-	vector<char> first_string(first_string_size * 2, '_');
-	vector<char> second_string(second_string_size * 2, '_');
+	list <char> first_string;
+	list <char> second_string;
 
-	for(int i = 1; i <= (int)first_string.size(); i+=2)
+	for(int i = 0; i < first_string_size; i++)
 	{
-		cin >> first_string[i];
+		cin >> c;
+		first_string.push_back(c);
 	}
-	for(int i = 1; i <= (int)first_string.size(); i+=2)
+	reverse(first_string.begin(),first_string.end());
+
+	for(int i = 0; i < second_string_size; i++)
 	{
-		cin >> second_string[i];
+		cin >> c;
+		second_string.push_back(c);
+	}
+
+	c = first_string.back();
+	list <char>::iterator itor_pos = second_string.begin();
+
+	for(int i = 0; i < sec; i++)
+	{
+		itor_pos++;
+	}
+
+	while(sec--)
+	{
+		c = first_string.back();
+		second_string.insert(itor_pos, c);
+		itor_pos--;
+		first_string.pop_back();
+
 	}
 	for(auto loop : first_string)
 	{
-		cout << loop;
+		cout <<loop << " ";
 	}
+	cout << "\n";
+
+	for(auto loop : second_string)
+	{
+		cout <<loop << " ";
+	}
+	cout << "\n";
 
 
-	return 0;
 }
