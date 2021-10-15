67913781dc70af4ebed3b54fc557c8d7007ebc83
KMJ <krocd@naver.com>
2021-10-15 19:22:15 +0900

    count sequencial candy in arry

--- Code_plus/foundaional_algorithm2/brute_force/candygame3085/cnadygame3085.py
+++ Code_plus/foundaional_algorithm2/brute_force/candygame3085/cnadygame3085.py
@@ -1,36 +1,36 @@
 import sys
 
-
+def check(arr):
+	len_arr = len(arr)
+	ans = 1
+	for i in range(len_arr):
+		count = 1
+		for j in range(1, len_arr):
+			if arr[i][j] == arr[i][j - 1]:
+				count+=1
+			else:
+				count = 1
+			if count > ans:
+				ans = count
+		count = 1
+		for k in range(1, len_arr):
+			if arr[k][i] == arr[k - 1][i]:
+				count +=1
+			else:
+				count = 1
+			if count > ans:
+				ans = count
+	return ans;
 
 
 def main():
 	n = int(input())
 	arr = list()
-
 	for _ in range(n):
 		arr.append(list(map(str,sys.stdin.readline().strip())))
 
-	sub_list = list()
-	count_candy = 0
-	for i in range(n):
-		ch = arr[i][0]
-		sub_list = list()
-		sub_list.append(ch)
-
-		for j in range(1, n):
-
-			if ch == arr[i][j]:
-				sub_list.append(ch)
-			else:
-				sub_list = list()
-				ch = arr[i][j]
-				sub_list.append(ch)
-		if len(sub_list) > 1:
-			count_candy += len(sub_list)
-
 	for i in range(n):
 		print(arr[i])
-	print(count_candy)
 
 
 if __name__ == "__main__":
