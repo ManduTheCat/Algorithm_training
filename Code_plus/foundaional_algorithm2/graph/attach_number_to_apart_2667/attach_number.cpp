94499e4448a3efea720091eb5a8a565bf226408a
kmj desktop <krocd@naver.com>
2021-10-31 23:01:53 +0900

    solved attach_number

--- Code_plus/foundaional_algorithm2/graph/attach_number_to_apart_2667/attach_number.cpp
+++ Code_plus/foundaional_algorithm2/graph/attach_number_to_apart_2667/attach_number.cpp
@@ -3,45 +3,12 @@
 #include <string.h>
 #include <queue>
 #include <utility>
-<<<<<<< HEAD
-=======
-
->>>>>>> refs/remotes/origin/master
+#include <algorithm>
 using namespace std;
 
 
 void bfs(int n, int **&arr, int **&check)
 {
-<<<<<<< HEAD
-	queue <pair<int, int>> q;
-	int x = 0;
-	int y = 0;
-
-	q.push(make_pair(x,y));
-	while(!q.empty())
-	{	
-		for(int i = 0; i < n; i++)
-		{
-			for(int j = 0; j < n; j++)
-			{
-				printf("%d" ,arr[i][j]);
-			}
-			printf("\n");
-		}
-
-		printf("\n");
-
-		for(int i = 0; i < n; i++)
-		{
-			for(int j = 0; j < n; j++)
-			{
-				printf("%d" ,check[i][j]);
-			}
-			printf("\n");
-		}
-	}
-
-=======
 	int dx[4] = {0, 0, 1, -1};
 	int dy[4] = {1, -1, 0, 0};
 	int x;
@@ -50,45 +17,48 @@ void bfs(int n, int **&arr, int **&check)
 	int group_count = 0;
 
 	queue <pair<int, int>> q;
+	vector <int> group_by_count;
 
 	for(int i = 0; i < n; i++)
 	{
 		for(int j = 0; j< n; j++)
 		{
-			x = i;
-			y = j;
 			if(check[i][j] == 0 && arr[i][j] == 1)
 			{
 				group_count++;
-				q.push(make_pair(x,y));
-				check[x][y] = group_count;
+				q.push(make_pair(i,j));
+				check[i][j] = group_count;
+				count = 1;
 				while(!q.empty())
 				{
 					x = q.front().first;
 					y = q.front().second;
 					q.pop();
-					count ++;
-					printf("x: %d,y: %d, count: %d\n", x, y, count);
+					// printf("x: %d,y: %d, count: %d\n", x, y, count);
 					for(int d = 0; d < 4; d++)
 					{
 						int nx;
 						int ny;
 						nx = x + dx[d];
 						ny = y + dy[d];
-						if(nx < n && nx >= 0 && ny < n && ny >= 0)
+						if(nx < n && nx >= 0 && ny < n && ny >= 0 && check[nx][ny] == 0 && arr[nx][ny] == 1)
 						{
-							if(check[nx][ny] == 0 && arr[nx][ny] == 1)
-							{
-								q.push(make_pair(nx, ny));
-								check[nx][ny] = group_count;
-							}
+							q.push(make_pair(nx, ny));
+							check[nx][ny] = group_count;
+							count++;
 						}
 					}
 				}
+				group_by_count.push_back(count);
 			}
 		}
 	}
->>>>>>> refs/remotes/origin/master
+	std::sort(group_by_count.begin(), group_by_count.end());
+	std::printf("%d\n", group_count);
+	for(auto loop : group_by_count)
+	{
+		std::printf("%d\n", loop);
+	}
 }
 
 
@@ -107,7 +77,6 @@ int main()
 	{
 		memset(check[i] , 0 , sizeof(int)*n);
 	}
-
 	for(int i = 0; i < n; i++)
 	{
 		for(int j = 0; j < n; j++)
@@ -115,25 +84,5 @@ int main()
 			scanf("%1d", &arr[i][j]); // 입력이 연속적일때 하나씩으로 분리하는 방법
 		}
 	}
-
-	for(int i = 0; i < n; i++)
-	{
-		for(int j = 0; j < n; j++)
-		{
-			printf("%d", arr[i][j]);
-		}
-		printf("\n");
-	}
 	bfs(n , arr, check);
-	std::printf("\n");
-
-	for(int i = 0; i < n; i++)
-	{
-		for(int j = 0; j < n; j++)
-		{
-			printf("%d", check[i][j]);
-		}
-		printf("\n");
-	}
-
 }
