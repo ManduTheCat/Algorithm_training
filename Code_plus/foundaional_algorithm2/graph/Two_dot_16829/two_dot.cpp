a8cf719eec54c9a396c82508d7b45264f70a9766
KMJ <krocd@naver.com>
2021-11-07 19:42:24 +0900

    bfs 만만듦듦

--- Code_plus/foundaional_algorithm2/graph/Two_dot_16829/two_dot.cpp
+++ Code_plus/foundaional_algorithm2/graph/Two_dot_16829/two_dot.cpp
@@ -2,8 +2,48 @@
 #include <vector>
 #include <queue>
 #include <string.h>
+#include <utility>
 
 using namespace std;
+
+void bfs_solution(int n, int m, char **&arr, int **&check)
+{
+	int dx[4] = {0, 0, 1, -1};
+	int dy[4] = {1, -1, 0 ,0};
+	int nx, ny;
+	int x, y;
+	char alpha;
+	queue <pair<int, int>> q;
+
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < m; j++)
+		{
+			if(check[i][j] == 0)
+			{
+				alpha = arr[i][j];
+				q.push(make_pair(i, j));
+				check[i][j] = 1;
+				while(!q.empty())
+				{
+					x = q.front().first;
+					y = q.front().second;
+					q.pop();
+					for(int d = 0; d < 4; d++)
+					{
+						nx = x + dx[d];
+						ny = y + dy[d];
+						if(nx >= 0 && ny >= 0 && n > nx && x > ny && check[nx][ny] == 0 && arr[nx][ny] == alpha)
+						{
+							q.push(make_pair(nx, ny));
+							check[nx][ny] = 1;
+						}
+					}
+				}
+			}
+		}
+	}
+}
 
 int main()
 {
@@ -35,6 +75,8 @@ int main()
 		}
 		printf("\n");
 	}
+
+	bfs_solution(n, m, arr, check);
 
 
 	return 0;
