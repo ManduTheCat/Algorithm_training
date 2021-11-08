8f0a703da8df8b9a9c3dd03768fd533829f59878
KMJ <krocd@naver.com>
2021-11-08 18:38:06 +0900

    process

--- Code_plus/foundaional_algorithm2/graph/Subway_line2_16947/Subway_line2.cpp
+++ Code_plus/foundaional_algorithm2/graph/Subway_line2_16947/Subway_line2.cpp
@@ -4,7 +4,37 @@
 
 using namespace std;
 
-vector <int> adj_list[51];
+
+
+//https://ongveloper.tistory.com/276 참조
+// 트리는 노드r갯수 -1 개의 간선을 가지게 된다. 이점을 이용해보자.
+vector <int> adj_list[3001];
+int is_in_cycle[3001]; // yes = true, no = false
+int visit_check[3001];
+int pre_vertex[3001];
+int check_status;
+
+void chek_is_circle(int cur_vertex)
+{
+	visit_check[cur_vertex] = 1;
+	for(int i = 0; i < (int)adj_list[cur_vertex].size(); i++)
+	{
+		if(check_status == 1)
+			return;
+
+		int next = adj_list[cur_vertex][i];
+
+		if(visit_check[next] == 1)
+		{
+
+		}
+		else
+		{
+
+		}
+	}
+
+}
 
 int main()
 {
