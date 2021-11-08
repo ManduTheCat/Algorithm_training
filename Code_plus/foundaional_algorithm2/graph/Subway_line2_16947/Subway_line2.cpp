218a498c58b2bae29273c1e8322f7523a42a11fa
KMJ <krocd@naver.com>
2021-11-08 18:51:55 +0900

    make dfs

--- Code_plus/foundaional_algorithm2/graph/Subway_line2_16947/Subway_line2.cpp
+++ Code_plus/foundaional_algorithm2/graph/Subway_line2_16947/Subway_line2.cpp
@@ -9,7 +9,7 @@ using namespace std;
 //https://ongveloper.tistory.com/276 참조
 // 트리는 노드r갯수 -1 개의 간선을 가지게 된다. 이점을 이용해보자.
 vector <int> adj_list[3001];
-int is_in_cycle[3001]; // yes = true, no = false
+int is_cycle_node[3001]; // yes = true, no = false
 int visit_check[3001];
 int pre_vertex[3001];
 int check_status;
@@ -24,12 +24,26 @@ void chek_is_circle(int cur_vertex)
 
 		int next = adj_list[cur_vertex][i];
 
-		if(visit_check[next] == 1)
+		if(visit_check[next] == 1) // 돌다가 방문햇던걸 다시 돌아온다오면
 		{
+			if(next != pre_vertex[cur_vertex]) //그게 바로 이전것이 아니라면
+			{
+				check_status = 1;
+				is_cycle_node[cur_vertex] = 1;
+				while(cur_vertex != next) // 과거 것을 체크 하면서 뒤로 간다;
+				{
+					is_cycle_node[pre_vertex[cur_vertex]] = 1;
+					cur_vertex = pre_vertex[cur_vertex];
+
+				}
+				return;
+			}
 
 		}
 		else
 		{
+			pre_vertex[next] = cur_vertex; // next 의 이전 vertex 기록
+			chek_is_circle(next);
 
 		}
 	}
