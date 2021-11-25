90a65755411c1e49230fd78270147d4f2911712e
kmj desktop <krocd@naver.com>
2021-11-25 18:59:49 +0900

    solved but memory over

--- Code_plus/foundaional_algorithm2/bfs/emoji_14226/emogi.cpp
+++ Code_plus/foundaional_algorithm2/bfs/emoji_14226/emogi.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 #include <utility>
 #include <tuple>
@@ -8,75 +7,67 @@ using namespace std;
 #define MAX 1000
 
 int n;
-int emoge = 1;
-int check_bfs[MAX * 2 + 1];
+int check_bfs[MAX + 1];
+
+bool check_target(int src, int target)
+{
+	if(src == target)
+	{
+		check_bfs[src] = 1;
+		return true;
+	}
+	return false;
+}
 
 void bfs(int target_num)
 {
 	queue <tuple<int, int, int>> q;
-	check_bfs[emoge] = 1;
-	q.push({emoge, 0, 0});
+	q.push({1, 0, 0});
 	while(!q.empty())
 	{
-		emoge = get<0>(q.front());
+		int emoge = get<0>(q.front());
 		int clip_bord = get <1> (q.front());
 		int	count = get <2> (q.front());
 		q.pop();
+		count++;
+		if(emoge > 0) // 복사
+		{
+			int temp_clip_bord = emoge;
+			// cout <<"{emoge , clipbord, count} : "<< emoge<< " , " << clip_bord <<" , "<< count <<"\n";
+			if(check_target(emoge, target_num))
+			{
+				cout << count << "\n";
+				break;
+			}
 
-		// if(emoge == target_num)
-		// {
-		// 	cout << "touch!! :" << emoge <<" count :" << count << "\n";
-		// 	check_bfs[target_num] = 1;
-		// 	break;
-		// }
-		if(emoge > 0) // 복
-		{
-			count++;
-			clip_bord = emoge;
-			cout <<"{emoge , clipbord, count} : "<< emoge<< " , " << clip_bord <<" , "<< count <<"\n";
-			if(emoge == target_num)
-			{
-				cout << "touch!! count = " << count << "\n";
-				check_bfs[target_num] = 1;
-				break;
-			}
-			q.push({emoge, clip_bord, count});
+			q.push({emoge, temp_clip_bord, count});
 			check_bfs[emoge] = 1;
 		}
-		if(emoge + clip_bord <= 1000 && clip_bord > 0) // 붙
+		if(emoge + clip_bord <= 1000 && clip_bord > 0) // 붙이기
 		{
-			count++;
-			emoge += clip_bord;
-			cout <<"{emoge , clipbord, count} : "<< emoge<< " , " << clip_bord <<" , "<< count <<"\n";
-			if(emoge == target_num)
+			int temp_emoge = emoge + clip_bord;
+			// cout <<"{emoge , clipbord, count} : "<< emoge<< " , " << clip_bord <<" , "<< count <<"\n";
+			if(check_target(temp_emoge , target_num))
 			{
-				cout << "touch!! count = " << count << "\n";
-				check_bfs[target_num] = 1;
+				cout << count << "\n";
 				break;
 			}
-			if(check_bfs[emoge] == 1)
-				continue;
-			q.push({emoge, clip_bord, count});
-			check_bfs[emoge] = 1;
+
+			check_bfs[temp_emoge] = 1;
+			q.push({temp_emoge, clip_bord, count});
 		}
-		if(emoge - 1 > 1) //잘
+		if(emoge - 1 > 1) //자르기
 		{
-			count++;
 			emoge --;
-			cout <<"{emoge , clipbord, count} : "<< emoge<< " , " << clip_bord <<" , "<< count <<"\n";
-			if(emoge == target_num)
+			// cout <<"{emoge , clipbord, count} : "<< emoge<< " , " << clip_bord <<" , "<< count <<"\n";
+			if(check_target(emoge, target_num))
 			{
-				cout << "touch!! count = " << count << "\n";
-				check_bfs[target_num] = 1;
+				cout << count << "\n";
 				break;
 			}
-
-			if(check_bfs[emoge] == 1)
-				continue;
 			q.push({emoge, clip_bord, count});
 			check_bfs[emoge] = 1;
 		}
-
 	}
 }
 
