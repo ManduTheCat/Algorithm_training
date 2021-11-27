6e104460ded0b2b3d75a00053319e2a5d12ae1b4
ManduTheCat <krocd@naver.com>
2021-11-27 10:01:45 +0900

    solved 1152

--- java_training/src/Algorithm_problem/CountWord_1152/CountWord.java
+++ java_training/src/Algorithm_problem/CountWord_1152/CountWord.java
@@ -5,12 +5,24 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 public class CountWord {
     public String[] stringArr;
+    public int count = 0;
     public void counting() throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         this.stringArr = bf.readLine().split(" ");
+//        System.out.println(stringArr.length);
+        for(String loop : stringArr) {
+//            System.out.println("str is :" + stringArr[i]);
+            int LenString = loop.length();
+            if(LenString > 0){
+                count++;
+            }
+        }
+        System.out.println(count);
     }
-    public static void main(String[] argv)
+    public static void main(String[] argv) throws IOException
     {
 
+        CountWord cw = new CountWord();
+        cw.counting();
     }
 }
