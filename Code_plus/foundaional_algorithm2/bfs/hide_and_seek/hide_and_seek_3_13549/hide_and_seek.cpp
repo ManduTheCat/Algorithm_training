7e6e3c5558126443490ef0009a665dd1277a2601
ManduTheCat <krocd@naver.com>
2021-12-02 16:59:57 +0900

    using dijkstra

---
+++ Code_plus/foundaional_algorithm2/bfs/hide_and_seek/hide_and_seek_3_13549/hide_and_seek.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int n;
+int m;
+
+void dijkstrea(){
+	priority_queue <pair<int, int>> pq;
+	vector <int> dist(n, 1000000);
+}
+
+int main(){
+	scanf("%d %d", &n, m);
+}
