705ed61b6d154720e0e71286b25bb79f9092876f
kmj desktop <krocd@naver.com>
2021-12-03 10:54:39 +0900

    problum is scope rangegit add *

--- Code_plus/foundaional_algorithm2/bfs/hide_and_seek/hide_and_seek_3_13549/hide_and_seek.cpp
+++ Code_plus/foundaional_algorithm2/bfs/hide_and_seek/hide_and_seek_3_13549/hide_and_seek.cpp
@@ -1,60 +1,57 @@
 //https://excited-hyun.tistory.com/33
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 #include <cstring>
 using namespace std;
 
-int n;
-int m;
+int dist[100001];
+int dx[2] = {1, -1};
 
-void dijkstrea(){
-	priority_queue <pair<int, int>> pq;
-	int dist[100001];
+priority_queue <pair<int, int>>  pq;
 
-	memset(dist, -1, sizeof(dist));
-	pq.push(make_pair(n, 0)); // node, cost;
-	dist[n] = 0;// 초기화;
+int main(){
+	int n;
+	int k;
+	
+	int cur_node;
+	int cur_cost;
+	int next_node;
+	int next_cost;
+
+	scanf("%d %d", &n, &k);
+
+	memset(dist, 1000000, sizeof(dist));
+	pq.push(make_pair(0, n));
+	dist[n] = 0;
 
 	while(!pq.empty()){
-		int cur_node = pq.top().first;
-		int cur_cost = -pq.top().second; // 저장할때 -로 pq 로 저장한다 c++ 우선순위 큐는 최대힙이다
+		
+		cur_cost = -pq.top().first;
+		cur_node = pq.top().second;
 		pq.pop();
 
-		if(dist[cur_node] < cur_cost){
+		if(dist[cur_node] < cur_cost)
+		{
 			continue;
 		}
 		for(int i = 0; i < 3; i++){
-			int next_node = 0;
-			int next_cost = 0;
-			if(i == 0){
-				next_node = cur_node + 1;
-				next_cost = cur_cost + 1;
-			}
-			else if(i == 1){
-				next_node = cur_node - 1;
-				next_cost = cur_cost + 1;
-			}
-			else if(i == 2){
-				next_node = cur_node * 2;
+			if(i == 2){
 				next_cost = cur_cost;
+				next_node = cur_node * 2;
 			}
-
-			if(next_node < 0  || next_node > 100000){
-				continue;
+			else{
+				next_cost = cur_cost + 1;
+				next_node = cur_node + dx[i];
 			}
-
-			if(dist[next_node] == -1 || dist[next_node] >  next_cost){
-				dist[next_node] = next_cost;
-				pq.push(make_pair(next_node, -next_cost));
+		if(next_node < 0 || next_node > 100000)
+			continue;
+		
+		if(dist[next_node] > next_cost){
+			dist[next_node] = next_cost;
+			pq.push(make_pair(-next_cost, next_node));
 			}
 		}
 	}
-	printf("output : %d\n", dist[m]);
-	return;
-}
-
-int main(){
-	scanf("%d %d", &n, &m);
-	dijkstrea();
+	printf("%d\n", dist[k]);
 }
