231677642d6fdbd258c29a6872ed6ffddf3064b0
ManduTheCat <krocd@naver.com>
2021-12-12 19:21:21 +0900

    add test case and del description

--- back_jun/parial_sum_1806/partial.cpp
+++ back_jun/parial_sum_1806/partial.cpp
@@ -6,29 +6,22 @@ using namespace std;
 
 int n;
 int s;
-int input[100000];
-// vector <pair<int, int>> res;
+vector <int> input;
 vector <int> res;
 
 void two_point(){
 	int end = 0;
 	int start = 0;
 	int partial_sum =0;
-	while(end != n){
+	while(end <= n){
 		if(partial_sum >= s){
-			// printf("%d ", partial_sum);
-			// res.push_back({start, end - 1});
 			res.push_back(end -start);
 			partial_sum -= input[start++];
-
-
 		}
 		else if(partial_sum  < s){
 			partial_sum+= input[end++];
 		}
 	}
-
-
 }
 
 int main(){
@@ -36,23 +29,12 @@ int main(){
 	for(int i = 0; i < n; i++){
 		int num;
 		scanf("%d", &num);
-		input[i] = num;
+		input.push_back(num);
 	}
 
 	two_point();
-	// printf("\n");
 
-	// for(auto loop : input){
-	// 	printf("%d ",loop);
-	// }
-	// printf("\n");
-	// for(int i = 0; i < (int)res.size(); i++){
-			// int first = res[i].first;
-			// int second = res[i].second;
-			// printf("{%d %d}", first, second);
-			// printf("%d ", res[i]);
-	// }
-	if(res.size() == 0){
+	if((int)res.size() == 0){
 		printf("0");
 	}
 	else{
