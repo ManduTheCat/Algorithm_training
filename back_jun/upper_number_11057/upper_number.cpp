c056109d18a533ff89deeaf8538efa8695b6057a
ManduTheCat <krocd@naver.com>
2021-12-17 07:22:51 +0900

    solve

--- back_jun/upper_number_11057/upper_number.cpp
+++ back_jun/upper_number_11057/upper_number.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
+#define MOD 10007
 
 using namespace std;
 
 int n;
-int count;
-void dp(int n, int num){
-	// n -1 은 n 보다 작거나 같은 수가오는게 가능하다.
-	if(n == 0){
-		return;
-	}
-	for(int i =0; i < 10; i++){
-		if(i <= num){
-			count++;
-
-			dp(n -1, i);
-		}
-	}
-
-}
+int dp[100000][10];
 int main(){
 	cin >> n;
-	dp(1, 9);
-	cout << count <<"\n";
-
+	for(int i = 0; i < 10; i++){
+		dp[1][i] = 1;
+	}
+	for(int i = 1; i <= n; i++){
+		for(int j = 0; j < 10; j++){
+			for(int k = 0; k <= j; k++){
+				dp[i][j] += dp[i - 1][k];
+				dp[i][j] %= MOD;
+			}
+		}
+	}
+	int sum = 0;
+	for(int j =0; j < 10; j++){
+		sum += dp[n][j] %MOD;
+	}
+	cout << sum % MOD<< "\n";
 }
