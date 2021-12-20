e96143e6f3ef89fbc8e462ebb4151663d536d299
kmj desktop <krocd@naver.com>
2021-12-21 00:20:58 +0900

    wine

---
+++ back_jun/wine/wine.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n;
+vector <int> wine;
+int main(){
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		int temp;
+		cin >> temp;
+		wine.push_back(temp);
+	}
+	for(int i = 0; i < n; i++){
+
+	}
+}
