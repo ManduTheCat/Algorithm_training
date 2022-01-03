bc387a81d3c06c407259d065b42bee9209628404
ManduTheCat <krocd@naver.com>
2022-01-03 19:47:16 +0900

    add pseudo code

---
+++ back_jun/greedy_panda_1937/panda.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+# define MAX 1000001
+
+using namespace std;
+
+// 백터 선언
+vector <vector<int>> bambo [MAX];
+
+// 시작 지점별값저장할 arr 선언
+int res[MAX];
+
+// 이전값을 기억하는 check 선언
+int check[MAX];
+
+// dfs 함수
+
+void dfs(int node){
+
+}
+
+
+int main(){
+int n;
+cin  >> n;
+// 입력 루프
+for(int i = 0; i < n; i++){
+	for(int  j  = 0; j < n; j++){
+
+	}
+}
+
+
+// dfs 시행 루프
+for(int i = 0; i< n; i++){
+	for(int j = 0; j < n; j++){
+		// check 초기화
+
+	}
+}
+
+}
