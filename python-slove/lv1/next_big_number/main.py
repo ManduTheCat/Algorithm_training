c9e6d46c963d561451e8d779ad6025380af0343c
ManduTheCat <krocd@naver.com>
2022-02-06 19:06:49 +0900

    add 다음 큰숫자

---
+++ python-slove/lv1/next_big_number/main.py
@@ -0,0 +1,7 @@
+import solution
+
+def main():
+	solution.solution(78)
+
+if __name__ == "__main__":
+	main()
---
+++ python-slove/lv1/next_big_number/solution.py
@@ -0,0 +1,6 @@
+def solution(n):
+	bin_n = bin(n)
+	for bin_ele in bin_n:
+		print(bin_ele)
+	answer = 0
+	return answer
