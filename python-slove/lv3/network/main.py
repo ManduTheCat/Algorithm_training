2ba7a23dfc430fc8f902e496cba4516dfe5ab355
ManduTheCat <krocd@naver.com>
2022-02-08 18:40:00 +0900

    solve network by python

---
+++ python-slove/lv3/network/main.py
@@ -0,0 +1,10 @@
+import solution
+
+def main():
+	input1 = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+	input2 = [[1, 1, 0], [1, 1, 1], [0, 1, 1]]
+	print(solution.solution(input1))
+	print(solution.solution(input2))
+
+if __name__ == "__main__":
+	main()
---
+++ python-slove/lv3/network/solution.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def solution(computers):
+	ans = 0
+	check = [False for i in range(1, 201)]
+	for i in range(len(computers)):
+		if check[i] == False:
+			dfs(i, computers, check)
+			ans +=1
+	return ans
+
+def dfs(node, computers, check):
+	q = deque()
+	check[node] = True
+	q.appendleft(node)
+	while not len(q) == 0:
+		cur_node = q[-1]
+		q.pop()
+		for i in range(len(computers[cur_node])):
+			if check[i] == False and computers[cur_node][i] == 1 and cur_node != i:
+				check[i] = True
+				q.appendleft(i)
